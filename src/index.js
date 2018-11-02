ae112321632682a1e21b41d26929729eb6892078
Micah Johnson <micahj2005@hotmail.com>
2018-11-02 16:34:10 -0500

    set up intial index reduxStore and provider

--- src/index.js
+++ src/index.js
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const reduxStore = createStore(
+    combineReducers({
+      
+    }),
+    applyMiddleware(logger)
+  )
+  
+  ReactDOM.render(
+    <Provider store={reduxStore}>
+      <App />
+    </Provider>, 
+    document.getElementById('root'));
 registerServiceWorker();

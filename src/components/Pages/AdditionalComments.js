26f514435acc5e94b3f3e3084c399536c2aea110
Micah Johnson <micahj2005@hotmail.com>
2018-11-04 15:47:28 -0600

    fixed textField issue, now it is functioning

--- src/components/Pages/AdditionalComments.js
+++ src/components/Pages/AdditionalComments.js
@@ -4,8 +4,7 @@ import { connect } from 'react-redux';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
-import Select from '@material-ui/core/Select';
-import MenuItem from '@material-ui/core/MenuItem';
+import TextField from '@material-ui/core/TextField';
 import FormControl from '@material-ui/core/FormControl';
 import Button from '@material-ui/core/Button';
 
@@ -45,14 +44,7 @@ class AdditionalComments extends Component {
             <Typography color="textPrimary" variant="h3">Any comments you want to leave? </Typography>
             <br></br>
             <FormControl onSubmit={this.handleSubmit}>
-                <Select onChange={this.handleChange} type="text" value={this.state.comments} placeholder="Add Comment Here" name="comments">
-                    <MenuItem value=''>select</MenuItem>
-                    <MenuItem value={1}>1</MenuItem>
-                    <MenuItem value={2}>2</MenuItem>
-                    <MenuItem value={3}>3</MenuItem>
-                    <MenuItem value={4}>4</MenuItem>
-                    <MenuItem value={5}>5</MenuItem>
-                </Select>
+                <TextField onChange={this.handleChange} type="text" value={this.state.comments} placeholder="Add Comment Here" name="comments"></TextField>
                 <br></br>
                 <Button onClick={this.handleSubmit} variant="contained" color="primary" type="submit" value="NEXT">NEXT</Button>
             </FormControl>

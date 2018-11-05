fcf1908b119c3e3c42b4db021c602bd2932975e9
Micah Johnson <micahj2005@hotmail.com>
2018-11-05 17:03:50 -0600

    removed /build from gitignore

---
+++ build/precache-manifest.d7bc1f7fc1318827a0ff326ac40a8f47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "fd106b67e9a8268247b6",
+    "url": "/static/js/main.fd106b67.chunk.js"
+  },
+  {
+    "revision": "1ad0545aa766146d8e13",
+    "url": "/static/js/1.1ad0545a.chunk.js"
+  },
+  {
+    "revision": "fd106b67e9a8268247b6",
+    "url": "/static/css/main.d3524602.chunk.css"
+  },
+  {
+    "revision": "1ad0545aa766146d8e13",
+    "url": "/static/css/1.ed1b272b.chunk.css"
+  },
+  {
+    "revision": "a04123c5d132b90193972a631f22ed5f",
+    "url": "/index.html"
+  }
+];
